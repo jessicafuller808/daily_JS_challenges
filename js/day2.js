1e7b4c86547e510b1efbb23c4c3c256d7b01500e
Jessica Fuller <jessicanfuller808@gmail.com>
2022-03-31 10:32:33 -0700

    finished day 2 problem

---
+++ js/day2.js
@@ -0,0 +1,9 @@
+// 8 kyu - Convert a string to an array
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+// Write a function to split a string and convert it into an array of words.
+
+function stringToArray(string){
+
+  return (string.split(' '));
+
+}
