16cafe35e9e691dd2a883981f8704f931e5f4542
Jessica Fuller <jessicanfuller808@gmail.com>
2022-04-02 09:41:34 -0700

    added function setup

--- js/day4.js
+++ js/day4.js
@@ -5,3 +5,6 @@
 * Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 * * years old". The first character in the string is always a number.
 */
 
+function getAge(inputString){
+    // return the girl's correct age as an integer. Happy coding :) 
+    }
