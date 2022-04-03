832963e34abf42eb79fba7627b7ab479e2e9a5b0
Jessica Fuller <jessicanfuller808@gmail.com>
2022-04-03 12:24:33 -0700

    .

--- js/day5.js
+++ js/day5.js
@@ -1,16 +1,12 @@
 /* *
 TODO Create a function that converts US dollars (USD) to Chinese Yuan (CNY) . The input is the amount of USD as an integer, and the output should be a string that states the amount of Yuan followed by 'Chinese Yuan'
-* Examples (Input -> Output)
-* 15  -> '101.25 Chinese Yuan'
-* 465 -> '3138.75 Chinese Yuan'
+Examples (Input -> Output)
+15  -> '101.25 Chinese Yuan'
+465 -> '3138.75 Chinese Yuan'
 * The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers should be represented as a string with 2 decimal places. (e.g. "21.00" NOT "21.0" or "21")
 */
 
 function usdcny(usd) {
-  let conversion = usd * 6.75;
-  console.log(`${conversion} Chinese Yuan`);
-}
-
-usdcny(5);
-usdcny(15);
-usdcny(3412);
+    let conversion = usd * 6.75;
+    return (`${conversion.toFixed(2)} Chinese Yuan`);
+  }
