1b2217382dda14204158f9b4b6de76916195b5de
Jessica Fuller <jessicanfuller808@gmail.com>
2022-04-04 04:59:25 -0700

    solved day6 problem

---
+++ js/day6.js
@@ -0,0 +1,10 @@
+/* *
+TODO Convert a number to a string
+* We need a function that can transform a number into a string.
+  What ways of achieving this do you know?
+*/
+
+function numberToString(num) {
+    // Return a string of the number here!
+    return num.toString();
+  }
