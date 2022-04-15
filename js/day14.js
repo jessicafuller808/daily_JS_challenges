78aaf1b842a56001367f497c22239f3e4e723737
Jessica Fuller <jessicanfuller808@gmail.com>
2022-04-14 21:46:46 -0700

    solved day14

---
+++ js/day14.js
@@ -0,0 +1,11 @@
+/* *
+* Opposite number
+TODO: Very simple, given an integer or a floating-point number, find its opposite.
+*/
+
+function opposite(number) {
+    return number - (number * 2);
+  }
+
+
+// console.log(opposite(6));
