700761fcc2673e7636a945107f3d0d24e5145f0d
Jessica Fuller <jessicanfuller808@gmail.com>
2022-04-19 19:15:37 -0700

    finished the 6kyu kata, day18.js

--- js/day18.js
+++ js/day18.js
@@ -20,22 +20,29 @@ const filtered = alphaArr.filter(([key, value]) => typeof value === 'string');
 
 
 
-// Accepts a string as a parameter
-function alphaPos(str) {
-    // Creates new string to transform str param into all lower case
-    let lowerStr = str.toLowerCase();
+let alphabet = {1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y', 26: 'z'};
+
+//converts the alphabet object into an array
+let alphaArr = Object.entries(alphabet);
+
+// filters the alphabet array and establishes sub arrays of the key value pairs converted to strings.
+const filtered = alphaArr.filter(([key, value]) => typeof value === 'string');
+
+
+function alphabetPosition(str) {
+    // transforms str to all lower case
+    str = str.toLowerCase();
     // setups a new string to house alphabet keys as strings
     let newStr = "";
     // iterates through the string's characters
-    for (i = 0; i < lowerStr.length; i++) {
+    for (i = 0; i < str.length; i++) {
         //for each string character, check if it's included in the alphabet array. If found, return the string at the 0 index (houses our alphabet obj).
         alphaArr.forEach(element => {
-            if (element.includes(lowerStr[i])) {
-                newStr += (element[0] + " ");
+            if (element.includes(str[i])) {
+                newStr += element[0] + ' ';
             }
         });
     }
-    console.log(newStr);
+    newStr = newStr.slice(0, -1);
+    return newStr;
 }
-
-alphaPos("The sunset sets at twelve o' clock.");
