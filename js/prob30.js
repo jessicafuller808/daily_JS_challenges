f550df9865fcb5b3961b6a744f8174c163b1ed8a
Jessica Fuller <jessicanfuller808@gmail.com>
2022-05-11 21:26:53 -0700

    added prob30

---
+++ js/prob30.js
@@ -0,0 +1,10 @@
+/**
+ * 8kyu - Remove string spaces
+ * TODO: Simple, remove the spaces from the string, then return the resultant string.
+ */
+
+ function noSpace(x){
+
+}
+
+noSpace('8 j 8   mBliB8g  imjB8B8  jl  B') // 8j8mBliB8gimjB8B8jlB
