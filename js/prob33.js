38dc1d0624fcdcd86765bdd1dd54fb1dfcf8ed76
Jessica Fuller <jessicanfuller808@gmail.com>
2022-05-16 11:52:25 -0700

    finished prob33

---
+++ js/prob33.js
@@ -0,0 +1,10 @@
+/**
+ * 8kyu - Convert a String to a Number
+ * TODO: We need a function that can transform a string into a number. What ways of achieving this do you know?
+ */
+
+ const stringToNumber = function(str){
+    return parseInt(str);
+  }
+
+  console.log(stringToNumber('12345'));
