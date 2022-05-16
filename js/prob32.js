ab24ec894bedea81cd7a098f45ffcda0655d62b4
Jessica Fuller <jessicanfuller808@gmail.com>
2022-05-15 21:55:55 -0700

    solved prob32

---
+++ js/prob32.js
@@ -0,0 +1,10 @@
+/**
+ * 8kyu - Function1 - Hello World
+ * TODO: Make a simple function called greet that returns the most-famous "hello world!".
+ */
+
+function greet() {
+    return 'hello world!';
+}
+
+console.log(greet());
