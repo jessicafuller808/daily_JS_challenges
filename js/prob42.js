d966f6808c7fc14f07f806bfa2cc86c71e073660
Jessica Fuller <jessicanfuller808@gmail.com>
2022-06-04 21:13:53 -0700

    finished prob42

---
+++ js/prob42.js
@@ -0,0 +1,19 @@
+/**
+ * 8kyu - Are You Playing Banjo?
+ * TODO: Create a function which answers the question, "Are you playing banjo?"
+ * *If your name starts with the letter "R" or lower case "r", you are playing banjo.
+ * The function takes a name as its only argument, and returns one of the following strings:
+    name + " plays banjo" 
+    name + " does not play banjo"
+ */
+
+    function areYouPlayingBanjo(name) {
+        let firstletter = name.charAt(0);
+        if(firstletter === 'R' || firstletter === 'r') {
+            return name + ' plays banjo';
+        } else {
+            return name + " does not play banjo"
+        }
+      }
+
+      console.log(areYouPlayingBanjo("robert"));
