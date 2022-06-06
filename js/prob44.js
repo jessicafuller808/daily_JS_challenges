ba17cbda47a692b296ccb7e5bda7cda69a110911
Jessica Fuller <jessicanfuller808@gmail.com>
2022-06-06 12:32:31 -0700

    setup problem for prob44

---
+++ js/prob44.js
@@ -0,0 +1,13 @@
+/**
+ * * 8kyu - Find the remainder
+ * TODO: Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+ * * Division by zero should return NaN.
+ */
+
+ function remainder(n, m){
+    // Divide the larger argument by the smaller argument and return the remainder
+    
+  }
+
+  console.log(remainder(17,5)); //2
+  console.log(remainder(1, 0)); //NAN
