4f5a439d18cd63a9b29c7c0051881fc8dfb64ba1
Jessica Fuller <jessicanfuller808@gmail.com>
2022-07-17 08:47:33 -0700

    solved prob57

--- js/prob57.js
+++ js/prob57.js
@@ -1,3 +1,16 @@
 /**
+ * *7kyu - The Highest Profit Wins
+ * TODO: Write a function that returns both the minimum and maximum number of the given list/array.
  * 
  */
+
+ function minMax(arr){
+    let newArr = [];
+    newArr.push(Math.min(...arr));
+    newArr.push(Math.max(...arr));
+    return newArr;
+  }
+
+
+  console.log(minMax([1, 2, 3, 4, 5])); //[1, 5]
+  console.log(minMax([2334454, 5])); //[5, 2334454]
